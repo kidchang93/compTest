55011a87f8c6c0cc682a49a588b920965a2fe0ee
kidchang93 <lck930721@gmail.com>
2024-05-22 14:40:45 +0900

    조건부 렌더링

--- vue/vue-basic/src/App.vue
+++ vue/vue-basic/src/App.vue
@@ -1,34 +1,45 @@
 <template>
-  <div>{{ count }}</div>
-  <h1>Vue.js 라이프 사이클 테스트</h1>
+  <div>
+    <!-- <div v-if="isVisible" class="red"></div>
+    <div v-if="isVisible == true" class="blue"></div>
+    <div v-else="false" class="black"></div> -->
+
+    <!-- <div v-if="count > 1" class="red"></div>
+    <div class="blue"></div> -->
+    <!-- <button @click="count++">증가</button>
+    <button @click="count--">감소</button> -->
+
+    <div v-show="isVisible" class="red"></div>
+    <div v-show="!isVisible" class="blue"></div>
+    <div v-if="isVisible" class="black"></div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      isVisible: false,
       count: 0,
     };
-  },
-  beforeCreate() {
-    console.log("LifeCycle is beforeCreate: ", this.count);
-  },
-  created() {
-    console.log("LifeCycle is Created: ", this.count);
-    this.test();
-  },
-  // beforeMount() {
-  //   console.log("LifeCycle is beforeMount", document.querySelector("h1"));
-  // },
-  // mounted() {
-  //   console.log("LifeCycle is mounted", document.querySelector("h1"));
-  // },
-  methods: {
-    test() {
-      console.log("함수 호출");
-    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.red {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+.blue {
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+}
+.black {
+  width: 100px;
+  height: 100px;
+  background-color: black;
+}
+</style>
