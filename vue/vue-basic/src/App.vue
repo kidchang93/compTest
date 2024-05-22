4d515bb186267c83b3fb49114e8d7607451cbd62
kidchang93 <lck930721@gmail.com>
2024-05-22 15:14:57 +0900

    텍스트 보간법을 활용한 선언적 렌더링

--- vue/vue-basic/src/App.vue
+++ vue/vue-basic/src/App.vue
@@ -1,35 +1,12 @@
 <template>
-  <div>
-    <!-- v-for 디렉티브를 사용 -->
-    <li
-      v-for="(item, index) in sampleArray"
-      :key="item"
-      @click="openDialog(index)"
-    >
-      {{ item }} / {{ index }}
-    </li>
-    <li v-for="(user, index) in otherArray" :key="user.name">
-      {{ user.id }} / {{ user.name }} / {{ index }}
-    </li>
-    <!-- 기존 HTML 코드 작성 방식 -->
-    <!-- <li>{{ sampleArray[0] }}</li>
-    <li>{{ sampleArray[1] }}</li>
-    <li>{{ sampleArray[2] }}</li>
-    <li>{{ sampleArray[3] }}</li> -->
-  </div>
+  <div>{{ rawHtml }}</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      sampleArray: ["a", "b", "c", "d"],
-      otherArray: [
-        { id: 0, name: "john" },
-        { id: 1, name: "kim" },
-        { id: 2, name: "lee" },
-        { id: 3, name: "park" },
-      ],
+      rawHtml: "이것은 텍스트입니다.",
     };
   },
 };
