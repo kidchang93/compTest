3cddc217b0b346a2ba95d105a7cdc10a266b9434
kidchang93 <lck930721@gmail.com>
2024-05-22 15:25:36 +0900

    클래스 바인딩

--- vue/vue-basic/src/App.vue
+++ vue/vue-basic/src/App.vue
@@ -3,6 +3,11 @@
   <div>{{ rawHtml2 }}</div>
   <!-- 아래가 정상 작동 -->
   <h1 v-html="rawHtml2"></h1>
+  <!-- 아래 두개는 똑같다. 단지 좀 짧은 버전 -->
+  <h2 v-bind:class="{ active: isActive }">클래스 바인딩 테스트 입니다.</h2>
+  <h2 :class="{ active: isActive }">미쳤다</h2>
+  <button @click="change">버튼</button>
+  <!-- isActive 가 True 여야만 활성화가 된다. -->
 </template>
 
 <script>
@@ -11,9 +16,19 @@ export default {
     return {
       rawHtml: "이것은 텍스트입니다.",
       rawHtml2: '<span style="color:red">이것은 빨간색 이어야 합니다.</span>',
+      isActive: false,
     };
+  },
+  methods: {
+    change() {
+      this.isActive = !this.isActive;
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+h2.active {
+  color: green;
+}
+</style>
