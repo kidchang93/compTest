e451daaa44310f3bd9d1a25057b6cf4a1fa8cd2b
kidchang93 <lck930721@gmail.com>
2024-05-22 16:51:57 +0900

    computed 실습

--- vue/vue-basic/src/App.vue
+++ vue/vue-basic/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <!-- <button v-on:click="count++">인라인 핸들러</button>
-    <h1>{{ count }}</h1> -->
-    <!-- 메서드 핸들러 -->
-    <button v-on:click="changeName">메서드 핸들러</button>
-    <h1>{{ name }}</h1>
+    <h1>{{ text }}</h1>
+    <h1>changeText 함수 호출 값: {{ changeText() }}</h1>
+    <h1>changeText 함수 호출 값: {{ changeText() }}</h1>
+    <h1>changeText 함수 호출 값: {{ changeText() }}</h1>
   </div>
 </template>
 
@@ -12,15 +11,21 @@
 export default {
   data() {
     return {
-      count: 0,
-      name: "Vue.js",
+      text: "Computed Test 데이터 문구입니다.",
     };
   },
   methods: {
-    changeName() {
-      this.name = "변경된 텍스트 데이터 입니다.";
+    changeText() {
+      console.log("함수 호출");
+      console.log(this.text);
+      return this.text.split("").reverse().join("");
     },
   },
+  // methods 부분에 선언된 동일한 logic일때
+  // 캐싱 기능이 없는 methods 는 호출될때마다 console 값이 출력이 되었습니다.
+  // 반면에, computed는 캐싱기능이 있기 떄문에 methods 와 어떤 차이점이 있는지
+  // 주의깊게 살펴봐야한다.
+  computed: {},
 };
 </script>
 
