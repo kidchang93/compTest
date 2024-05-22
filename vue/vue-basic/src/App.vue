7c71c667da53fcbdf154a6b3bfc10e58139a4232
kidchang93 <lck930721@gmail.com>
2024-05-22 16:34:47 +0900

    인라인 핸들러

--- vue/vue-basic/src/App.vue
+++ vue/vue-basic/src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div><button>인라인 핸들러</button></div>
+  <div>
+    <button v-on:click="count++">인라인 핸들러</button>
+    <h1>{{ count }}</h1>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      data() {
-        return {};
-      },
+      count: 0,
     };
   },
 };
