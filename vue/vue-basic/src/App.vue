fbce502229f29a33752bc78fa9561d304c981f9e
kidchang93 <lck930721@gmail.com>
2024-05-22 15:19:53 +0900

    v-html 적용방식

--- vue/vue-basic/src/App.vue
+++ vue/vue-basic/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div>{{ rawHtml }}</div>
+  <div>{{ rawHtml2 }}</div>
+  <!-- 아래가 정상 작동 -->
+  <h1 v-html="rawHtml2"></h1>
 </template>
 
 <script>
@@ -7,6 +10,7 @@ export default {
   data() {
     return {
       rawHtml: "이것은 텍스트입니다.",
+      rawHtml2: '<span style="color:red">이것은 빨간색 이어야 합니다.</span>',
     };
   },
 };
