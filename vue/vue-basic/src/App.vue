33865467b0cb379e8333fd87d0dd95b8cf189c27
kidchang93 <lck930721@gmail.com>
2024-05-23 11:28:47 +0900

    OptionsAPI에서의 Props

--- vue/vue-basic/src/App.vue
+++ vue/vue-basic/src/App.vue
@@ -1,37 +1,27 @@
 <template>
   <div>
-    <button @click="changeMessage">{{ message }}</button>
-    {{ watchMessage }}
+    <ChildComponent
+      v-bind:sendProps1="title"
+      v-bind:sendProps2="createAt"
+      v-bind:sendProps3="obj"
+    ></ChildComponent>
   </div>
 </template>
 
 <script>
+import ChildComponent from "./components/ChildComponent.vue";
+
 export default {
+  components: { ChildComponent },
   data() {
     return {
-      message: "안녕하세요 . Vue.js WATCH 기능 테스트 오리지널 문구",
-      watchMessage: "",
+      title: "부모 컴포넌트에서 선언된 내용입니다.",
+      createAt: 2024,
+      obj: {
+        id: 2024,
+        name: "john",
+      },
     };
-  },
-  watch: {
-    // message : function(){}
-    // 데이터 뿐만 아니라 computed로 계산된 형태의 데이터도 watch로 감지할 수 있습니다.
-    // 보통 게시판에서 한 컬럼을 선택하였을때 고유한 ID 값이 바뀜을 감지하고
-    // 이때 그 ID 값에 따른 상세 데이터를 호출할 때 주로 사용한다.
-    message() {
-      // window.alert("message 변수에 담긴 데이터가 변경되었습니다.");
-      this.watchMessage = "watch 동작";
-    },
-    id() {
-      // 해당 상세데이터를 조회하는 api 호출
-    },
-  },
-
-  methods: {
-    changeMessage() {
-      console.log("함수 호출");
-      this.message = "변경된 message 데이터 입니다.";
-    },
   },
 };
 </script>
