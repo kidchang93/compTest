d17ae559573fc942a236db3aa475f918c21cdde8
kidchang93 <lck930721@gmail.com>
2024-05-23 14:03:20 +0900

    Composition API 환경에서 Props 사용하기

---
+++ vue/my-vue-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ChildComponent
+      :send-props1="title"
+      :send-props2="createdAt"
+      :send-props3="obj"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import ChildComponent from "./components/ChildComponent.vue";
+
+interface obj {
+  id: Number;
+  name: String;
+}
+
+const title = ref<String>("부모 컴포넌트에서 선언된 내용입니다.");
+const createdAt = ref<Number>(2024);
+const obj = reactive({
+  id: 2024,
+  name: "john",
+});
+</script>
+
+<style scoped></style>
---
+++ vue/my-vue-app/src/components/ChildComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{ props.sendProps1 }}</div>
+  <div>{{ props.sendProps2 }}</div>
+  <div>{{ props.sendProps3.id }}</div>
+  <div>{{ props.sendProps3.name }}</div>
+</template>
+
+<script setup lang="ts">
+interface obj {
+  id: Number;
+  name: String;
+}
+interface Props {
+  sendProps1: String;
+  sendProps2: Number;
+  sendProps3: obj;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped></style>
